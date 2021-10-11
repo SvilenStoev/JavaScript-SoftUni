ed23055e46701edb4bc52089e34c4af02274b8c8
Svilen Stoev <svilen.d.stoev@abv.bg>
2021-10-11 20:04:48 +0300

    05. DOM Manipulations and Events - added 07. shoping cart

--- JS Advanced/05. DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ JS Advanced/05. DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkOut);
+
+   const textAreaEl = document.getElementsByTagName('textarea')[0];
+   const cart = [];
+
+   function onClick(ev) {
+      if (ev.target.tagName == 'BUTTON' && ev.target.className == 'add-product') {
+         const prodEl = ev.target.parentNode.parentNode;
+         const name = prodEl.querySelector('.product-title').textContent;
+         const price = Number(prodEl.querySelector('.product-line-price').textContent);
+
+         cart.push({
+            name,
+            price
+         });
+
+         textAreaEl.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+      }
+   }
+
+   function checkOut() {
+      const uniqueProducts = new Set();
+      cart.forEach(p => uniqueProducts.add(p.name));
+
+      const totalPrice = cart.reduce((t, p) => t + p.price, 0);
+
+      textAreaEl.value += `You bought ${[...uniqueProducts.keys()].join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+      Array.from(document.querySelectorAll('button')).forEach(b => b.disabled = true);
+   }
 }
